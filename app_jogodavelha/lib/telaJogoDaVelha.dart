fe142191e56e7b5e9d6f17dba0fd6eba41268f0b
Rafael Ferreira Machado <106564051+RafaDorf04042022@users.noreply.github.com>
2023-11-28 22:14:52 -0300

    version 0.3

--- app_jogodavelha/lib/telaJogoDaVelha.dart
+++ app_jogodavelha/lib/telaJogoDaVelha.dart
@@ -10,18 +10,38 @@ class TelaJogoDaVelha extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Jogo da Velha'),
+          backgroundColor: Color(0xFF1F1F21),
+          title: Row(
+            children: [
+              Image.asset(
+                'images/x.png',
+                width: 30, // Ajuste o tamanho conforme necessário
+                height: 30,
+              ),
+              SizedBox(width: 8), // Espaçamento entre a imagem e o título
+              Image.asset(
+                'images/bola.png',
+                width: 30, // Ajuste o tamanho conforme necessário
+                height: 30,
+              ),
+            ],
+          ),
         ),
         backgroundColor: Color(0xFF1F1F21), // Cor de fundo padrão do app
-        body: Container(
-          width: 300,
-          height: 169,
-          child: Center(
-            child: Text(
-              'Conteúdo da Tela',
-              style: TextStyle(fontSize: 20),
-            ),
+        body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, // Número de colunas na grade
           ),
+          itemBuilder: (context, index) {
+            return IconButton(
+              icon: Icon(Icons.star),
+              onPressed: () {
+                // Lógica a ser executada quando o botão for pressionado
+                print('Botão $index pressionado!');
+              },
+            );
+          },
+          itemCount: 9, // Número total de ícones/botões
         ),
       ),
     );
